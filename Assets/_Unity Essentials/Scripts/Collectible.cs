273b7b998361d3385e9ac9831c90d7a535002b57
DeisyAlmeida <almeidadeisy2003@gmail.com>
2025-05-21 16:39:53 +0100

    Initial commit

---
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject onCollectEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0);
+
+    }
+    private void OnTriggerEnter(Collider outro)
+    {
+        // Destroy the collectible
+        Destroy(gameObject);
+
+        // instantiate the particle effect
+        Instantiate(onCollectEffect, transform.position, transform.rotation);
+    }
+}
